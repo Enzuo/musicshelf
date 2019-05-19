18b4449c4a1e0cf531b3a38a72b545665a266c94
Enzuo <potc.zone+git@gmail.com>
2019-05-19 19:07:32 +0200

    Save musics to chrome storage

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,12 +3,44 @@
 var settings = new Store("settings");
 
 
+var musics = [];
+chrome.storage.sync.get(['musics'], function(result) {
+  console.log('Value currently is ' + JSON.stringify(result, null, 2));
+  musics = result.musics;
+});
+
+
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log('settings', settings.get('gsheet'))
 
-  	if(request === 'saveMusic'){
+  	if(request.type === 'saveMusic'){
+      saveMusic(request.payload);
+    }
+
+    if(request.type === 'getMusic'){
+    }
+
+    if(request.type === 'getAllMusic'){
+      sendResponse(getAllMusic());
     }
   }
 );
+
+function saveMusic(music){
+  console.log('saveMusic');
+  if(!musics){
+    musics = [];
+  }
+
+  musics.push(music)
+
+  chrome.storage.sync.set({musics: musics}, function() {
+    console.log('Music saved ', music);
+  });
+}
+
+function getAllMusics(){
+  return musics;
+}
